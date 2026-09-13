c60bfe7a98820b6ad5d77e29a07ebdc95a568232
dinatharwat123 <158522794+dinatharwat123@users.noreply.github.com>
2026-09-13 22:55:15 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// ============================================
+// بيانات الاتصال بمشروع Supabase بتاعك
+// ============================================
+const SUPABASE_URL = "https://qdwzaftxnrhdgjisctjl.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_lKp3qizAsITLNnSl1aMt9w_BB3XpvMC";
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
